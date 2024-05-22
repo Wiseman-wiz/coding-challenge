00251d4ec6863e15ccfd29b3f9182e4e9e03ce4d
Wiseman-wiz <jhannuel.ramos@gmail.com>
2024-05-22 16:29:24 +0800

    latest solved problems

--- LeetCode/two-sum/twoSum.js
+++ LeetCode/two-sum/twoSum.js
@@ -1,7 +1,3 @@
-var a = [9, [2, 7, 11, 15]];
-var b = [6, [3, 2, 4]];
-var c = [6, [3, 3]];
-
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -16,10 +12,5 @@ var twoSum = function (target, nums) {
         }
         numberToIndex[nums[i]] = i;
     }
-
     return numberToIndex;
 };
-
-console.log(twoSum(a[0], a[1]));
-console.log(twoSum(b[0], b[1]));
-console.log(twoSum(c[0], c[1]));
