6d90b716caa804279f7c5ecd2d5379e1bb934af4
Wiseman-wiz <jhannuel.ramos@gmail.com>
2024-05-22 10:17:05 +0800

    initial commit

---
+++ LeetCode/twoSum.js
@@ -0,0 +1,26 @@
+
+var a = [9, [2, 7, 11, 15]];
+var b = [6, [3, 2, 4]];
+var c = [6, [3, 3]];
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (target, nums) {
+    var numberToIndex = {};
+    for (i = 0; i < nums.length; i++) {
+        var complement = target - nums[i];
+        if (complement in numberToIndex) {
+            return [numberToIndex[complement], i];
+        }
+        numberToIndex[nums[i]] = i;
+    }
+
+    return numberToIndex;
+};
+
+console.log(twoSum(a[0], a[1]));
+console.log(twoSum(b[0], b[1]));
+console.log(twoSum(c[0], c[1]));
